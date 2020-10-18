f64f912b8b278f7fe21a82e3469567a247c5fcb8
Michael Rojek <michaeltrojek@gmail.com>
2020-10-18 08:00:34 -0400

    Submit Session 3

--- course-04/exercises/c4-demos-master/01-simple-function/handler.js
+++ course-04/exercises/c4-demos-master/01-simple-function/handler.js
@@ -3,6 +3,7 @@
 exports.handler = async (event, context) => {
   console.log('Event: ', event)
   console.log('Context: ', context)
+  console.log('test');
   
   return {
       result: 'Hello Udacity!'
